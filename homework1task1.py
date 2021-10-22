934c1500153212264874f21ac1e06657f986383a
kirillgogol <kirill.gogol2002@gmail.com>
2021-10-22 21:46:17 +0300

    homework1

---
+++ homework1task1.py
@@ -0,0 +1,5 @@
+N = int(input())
+sec = N % 60
+minute = N // 60 % 60
+hour = N // (60 * 60)
+print(hour, minute, sec)
---
+++ homework1task2.py
@@ -0,0 +1,6 @@
+s = input()
+my_name = "Kirill"
+s = s.replace("Uncle Styopa", my_name)
+s = s.replace("Uncle", my_name)
+s = s.replace("Styopa", my_name)
+print(s)
